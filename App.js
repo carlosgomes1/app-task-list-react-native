0a403583ffd5139e8d1c2dab352cfc641564ee5a
carlosgomes1 <48812516+carlosgomes1@users.noreply.github.com>
2020-03-14 19:54:30 -0300

    App ok com asyncStorage pronto

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal, 
+  TextInput, AsyncStorage } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import TaskList from './src/components/TaskList/'
 import * as Animatable from 'react-native-animatable';
@@ -7,15 +8,52 @@ import * as Animatable from 'react-native-animatable';
 const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
-  const [task, setTaks] = useState([
+  const [ task, setTask ] = useState([]);
+  const [ open, setOpen ] = useState(false);
+  const [ input, setInput ] = useState('');
 
-    { key:1, task: "Ed lindo" },
-    { key:2, task: "Ed gatao" },
-    { key:3, task: "Ed foda mane" },
-    { key:4, task: "Caco feioso" },
-    { key:5, task: "ae ed xera pal" },
+  useEffect(() => {
+    async function loadTasks() {
+      const taskStorage = await AsyncStorage.getItem('@task');
 
-  ]);
+      if(taskStorage) {
+        setTask(JSON.parse(taskStorage));
+      }
+    }
+
+    loadTasks();
+
+  }, []);
+
+  useEffect(() => {
+
+    async function saveTask() {
+      await AsyncStorage.setItem('@task', JSON.stringify(task));
+    }
+
+    saveTask();
+
+  }, [task]);
+
+  function addTask() {
+    if( input === '' ) {
+      return
+    }
+
+    const data = {
+      key: input,
+      task: input
+    };
+
+    setTask([...task, data]);
+    setOpen(false)
+    setInput('');
+  }
+
+  const handleDelete = useCallback((data) => {
+    const find = task.filter(r => r.key !== data.key);
+    setTask(find);
+  })
 
   return(
     <SafeAreaView style={ styles.container }>
@@ -34,14 +72,47 @@ export default function App() {
         showsHorizontalScrollIndicator={ false }
         data={task}
         keyExtractor={(item) => String(item.key)}
-        renderItem={({ item }) => <TaskList data={item} /> }
+        renderItem={({ item }) => <TaskList data={item} handleDelete={ handleDelete } /> }
       />
+
+      <Modal animationType="slide" transparent={ false } visible={ open }>
+
+        <SafeAreaView style={ styles.modal }>
+
+          <View style={ styles.modalHeader } >
+            <TouchableOpacity onPress={ () => setOpen(false) } >
+              <Ionicons style={{ marginLeft: 5, marginRight: 5, }} name="md-arrow-back" size={ 40 } color="#FFF"/>
+            </TouchableOpacity>
+            <Text style={ styles.modalTitle }> Nova tarefa </Text>
+          </View>
+
+          <Animatable.View style={styles.modalBody} animation="fadeInUp" useNativeDriver>
+            <TextInput
+              multiline={true}
+              placeholderTextColor="#747474"
+              autoCorrect={false}
+              placeholder="Digite uma nova tarefa..."
+              style={ styles.input }
+              value={input}
+              onChangeText={ (text) => setInput(text) } 
+            />
+
+            <TouchableOpacity style={ styles.btnCadastrar } onPress={ addTask }>
+              <Text style={ styles.textBtnCadastrar } > Cadastrar </Text>
+            </TouchableOpacity>
+
+          </Animatable.View>
+
+        </SafeAreaView>
+
+      </Modal>
 
       <AnimatedBtn 
       style={ styles.fab }
       useNativeDriver
       animation="bounceInUp"
       duration={1500}
+      onPress={ () => setOpen(true) }
       >
         <Ionicons
           name="ios-add"
@@ -87,5 +158,58 @@ const styles = StyleSheet.create({
       width: 1,
       height: 3,
     },
+  },
+
+  modal: {
+    flex: 1,
+    backgroundColor: '#171D31',
+  },
+
+  modalHeader: {
+    marginLeft: 10,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  modalTitle: {
+    fontSize: 30,
+    marginLeft: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  modalBody: {
+    marginTop: 15,
+  },  
+
+  input: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 30,
+    height: 120,
+    backgroundColor: '#FFF',
+    padding: 9,
+    color: "#000",
+    borderRadius: 10,
+    textAlignVertical: 'top',
+  },
+
+  btnCadastrar: {
+    backgroundColor: '#FFF',
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40,
+    width: '94%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+
+  textBtnCadastrar: {
+    fontSize: 20,
   }
+
 });
